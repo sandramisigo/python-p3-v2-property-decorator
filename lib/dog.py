faa6a09f43803f1ba23d193444d8cb8f50265c0b
Sandra Misigo <misigosandra@gmail.com>
2025-02-25 18:38:39 +0300

    python-p3-v2-property-decorator

--- lib/dog.py
+++ lib/dog.py
@@ -14,18 +14,19 @@ class Dog:
     def __init__(self, name='Fido', breed='Mastiff'):
         self.name = name
         self.breed = breed
-
-    def get_name(self):
+    
+    @property
+    def name(self):
+        """The name property"""
         return self._name
 
-    def set_name(self, name):
+    @name.setter
+    def name(self, name):
+        """Name must be a string between 1 and 25 characters in length"""
         if isinstance(name, str) and 1 <= len(name) <= 25:
-            self._name = name.title()
+            self._name = name
         else:
-            raise ValueError(
-                "Name must be string between 1 and 25 characters.")
-
-    name = property(get_name, set_name)
+            raise ValueError("Name must be string between 1 and 25 characters." )
 
     def get_breed(self):
         return self._breed
@@ -36,4 +37,15 @@ class Dog:
         else:
             raise ValueError("Breed must be in list of approved breeds.")
 
-    breed = property(get_breed, set_breed)
+    @property
+    def breed(self):
+        """The breed property"""
+        return self._breed
+
+    @breed.setter
+    def breed(self, breed):
+        """Breed must be in the list of approved breeds"""
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            raise ValueError("Breed must be in list of approved breeds.")
